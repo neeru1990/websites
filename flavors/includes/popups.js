f0fa5eb887e560e45cc22ac16696e022064e09b6
Pushkar Modi <pushkar@gigwalk.com>
2013-05-05 11:42:21 -0700

    Code for flavorsttd.com

---
+++ flavors/includes/popups.js
@@ -0,0 +1,7 @@
+<!--
+function legal()
+{ open('legal.htm','legal','menubar=no,width=700,height=480,location=no,resizable=no,toolbar=no,scrollbars=yes') }
+
+function privacy()
+{ open('privacy.htm','privacy','menubar=no,width=700,height=480,location=no,resizable=no,toolbar=no,scrollbars=yes') }
+-->
